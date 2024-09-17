28c3003c213c4f9d904b2c7708fe99d342b5626e
shiyangzhang <shiyangzhang@whale.im>
2024-09-17 17:27:24 +0800

    [master]

--- youtube.js
+++ youtube.js
@@ -4,7 +4,7 @@
 // @version      2024-09-16
 // @description  try to take over the world!
 // @author       You
-// @match        https://www.youtube.com/watch?v=jKSS6FdiAWw
+// @match        https://www.youtube.com/watch*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
 // ==/UserScript==
@@ -28,6 +28,7 @@
     }
     const video = document.querySelector("video");
     video.currentTime = time;
+    video.play();
   };
   ws.onerror = function (evt) {
     console.log("ws:> error " + evt.data);
